aba040a757b27eab2737fc99d0907ef823cd13d9
Mario  Perna <marioperna@MBP-di-Mario.home-life.hub>
2019-06-15 01:03:29 +0200

    Code Indentation

--- index.php
+++ index.php
@@ -2,130 +2,130 @@
 <head>
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
   <style>
-  	#container
-    {
-    	width:98%;
-        height:98%;
-		
-        background-color: lightgray;
-        color: black;
-        font-family: Verdana, Serif;
-        font-size:12px;
-        float: left;
-        margin-left: auto;
-        margin-right:auto;
-     }
-  	#container_domande{
-    	height: 70%;
-        width: 25%;
-        float: left;
-        background-color: lightgray;
-        color: black;
-        font-family: Verdana, Serif;
-        font-weight: bold;
-        font-size:12px;
-        padding: 1%;
-        border:1px solid black;
-    }
-    .domanda
-    {
-    	text-decoration: none;
-    }
-    #infoAzienda
-    {
-        border:1px solid black;
-    	height: 20%;
-        width: 25%;
-        float:left;
-        padding:1%;
-    }
-    #infoTest
-    {
-        border:1px solid black;
-    	height: 20%;
-        width: 67%;
-      	float:left;
-        padding:1%;
-    }
-  	#mostraRisposte{
-    	height: 70%;
-        width: 67%;
-        border:1px solid black;
-        float: left;
-        padding:1%;
+  #container
+  {
+    width:98%;
+    height:98%;
 
-    }
-    .bloccoDomanda{
-    	
-        background-color: gray;
-       
-		
-    }
+    background-color: lightgray;
+    color: black;
+    font-family: Verdana, Serif;
+    font-size:12px;
+    float: left;
+    margin-left: auto;
+    margin-right:auto;
+  }
+  #container_domande{
+    height: 70%;
+    width: 25%;
+    float: left;
+    background-color: lightgray;
+    color: black;
+    font-family: Verdana, Serif;
+    font-weight: bold;
+    font-size:12px;
+    padding: 1%;
+    border:1px solid black;
+  }
+  .domanda
+  {
+    text-decoration: none;
+  }
+  #infoAzienda
+  {
+    border:1px solid black;
+    height: 20%;
+    width: 25%;
+    float:left;
+    padding:1%;
+  }
+  #infoTest
+  {
+    border:1px solid black;
+    height: 20%;
+    width: 67%;
+    float:left;
+    padding:1%;
+  }
+  #mostraRisposte{
+    height: 70%;
+    width: 67%;
+    border:1px solid black;
+    float: left;
+    padding:1%;
+
+  }
+  .bloccoDomanda{
+
+    background-color: gray;
+
+
+  }
   </style>
 </head>
 <body>
-<div id="container">
-	<div id="infoAzienda"></div>
+  <div id="container">
+    <div id="infoAzienda"></div>
     <div id="infoTest"></div>
-  <div id="container_domande">
-    <?php
-    require("connessione.php");
+    <div id="container_domande">
+      <?php
+      require("connessione.php");
 
-    $sql = "SELECT id, domanda FROM _webquiz";
-    $ris = mysql_query($sql) or die("errore esecuzione query");
+      $sql = "SELECT id, domanda FROM _webquiz";
+      $ris = mysql_query($sql) or die("errore esecuzione query");
 
-    while($row = mysql_fetch_array($ris))
-    {
-      echo "
-      
-      <a href=javascript:void(0); id=".$row["id"]." class='domanda' OnClick=\"leggiDom('".$row["id"]."')\">".$row["domanda"]."</a>
-      <hr/>";
-    }
-    ?>
-  </div>
-  <div id="mostraRisposte">
+      while($row = mysql_fetch_array($ris))
+      {
+        echo "
+
+        <a href=javascript:void(0); id=".$row["id"]." class='domanda' OnClick=\"leggiDom('".$row["id"]."')\">".$row["domanda"]."</a>
+        <hr/>";
+      }
+      ?>
+    </div>
+    <div id="mostraRisposte">
       <input type="checkbox" name="r[]" id="r1" value=""><label id="lab_r1"></label> <br>
       <input type="checkbox" name="r[]" id="r2" value=""><label id="lab_r2"></label>  <br>
       <input type="checkbox" name="r[]" id="r3" value=""><label id="lab_r3"></label>  <br>
       <input type="checkbox" name="r[]" id="r4"  value=""><label id="lab_r4"></label>  <br>
       <input type="hidden" id="idDom" name="idDom" value="">
-      
-       <textarea rows="4" cols="50" id="commento" onfocusout="inserisciCommento()"></textarea> 
-   </div>
-</div>
+
+      <textarea rows="4" cols="50" id="commento" onfocusout="inserisciCommento()"></textarea>
+    </div>
+  </div>
 
   <script>
-  
+
   $("#r1").hide();
   $("#commento").hide();
   $("#r2").hide();
   $("#r3").hide();
   $("#r4").hide();
-  
+
   $("#commento").hide();
-  
+
   function inserisciCommento()
   {
-  	       
-       var idDomanda = $('#idDom').val();
-//inizio chimaata ajax
-       var commento = $('#commento').val();    
-        $.ajax({
-          type: "POST",
-          url: "risposte.php",
-          data: {id_domanda: idDomanda, commento:commento},
-          dataType: "JSON",
-          success: function(esito)
-          {
-				//$("#c").css('border-color', '#green');
-          },
-          error: function()
-          {
-            
-          }
-        });
-        //fine chiamata ajax
-       
+
+    var idDomanda = $('#idDom').val();
+    //inizio chimaata ajax
+    var commento = $('#commento').val();
+    $.ajax({
+      type: "POST",
+      url: "risposte.php",
+      data: {id_domanda: idDomanda, commento:commento},
+      dataType: "JSON",
+      success: function(esito)
+      {
+        //$("#c").css('border-color', '#green');
+      },
+      error: function()
+      {
+
+      }
+    });
+    //fine chiamata ajax
+
   }
   function leggiDom(a)
   {
@@ -137,37 +137,37 @@
       dataType: "JSON",
       success: function(dati)
       {
-      $("#commento").val("");
-           $("#r1").show();
- 			$("#r2").show();
-  			$("#r3").show();
-  	 		$("#r4").show();
-    		$("#commento").show();
-		//carica le label
+        $("#commento").val("");
+        $("#r1").show();
+        $("#r2").show();
+        $("#r3").show();
+        $("#r4").show();
+        $("#commento").show();
+        //carica le label
         $("#lab_r1").text(dati.risposte[0]);
         $("#lab_r2").text(dati.risposte[1]);
         $("#lab_r3").text(dati.risposte[2]);
         $("#lab_r4").text(dati.risposte[3]);
         //alert(dati.infoDom[1]);
-		//carica i valori contenuti nelle checkboxes
+        //carica i valori contenuti nelle checkboxes
         $("#idDom").val(dati.infoDom[0]);
         //$("#tipoDom").val(dati.infoDom[2]);
-        
+
         $("#r1").val(dati.risposte[0]);
         $("#r2").val(dati.risposte[1]);
         $("#r3").val(dati.risposte[2]);
         $("#r4").val(dati.risposte[3]);
-        
-		//splitta l'array e prende le risposte 
+
+        //splitta l'array e prende le risposte
         var rispDate = dati.rispDate[1].split(",");
-        
-		//imposto tutte le checkbox che al cambio di domanda come non selezionate, poi il controllo 
+
+        //imposto tutte le checkbox che al cambio di domanda come non selezionate, poi il controllo
         //qui sotto effettuerà la verifica delle risposte date da utente
         $("#r1").prop('checked', false);
         $("#r2").prop('checked', false);
         $("#r3").prop('checked', false);
         $("#r4").prop('checked', false);
-        
+
         //inizio controllo se confermata la risposta 1
         if(dati.risposte[0]== rispDate[0])
         {$("#r1").prop('checked', true);}
@@ -181,7 +181,7 @@
         if(dati.risposte[3]== rispDate[0])
         {$("#r4").prop('checked', true);}
         //fine controllo se selezionata la risposta 1
-        
+
         //inizio controllo se confermata la risposta 2
         if(dati.risposte[0]== rispDate[1])
         {$("#r1").prop('checked', true);}
@@ -195,7 +195,7 @@
         if(dati.risposte[3]== rispDate[1])
         {$("#r4").prop('checked', true);}
         //fine controllo se selezionata la risposta 2
-        
+
         //inizio controllo se confermata la risposta 3
         if(dati.risposte[0]== rispDate[2])
         {$("#r1").prop('checked', true);}
@@ -209,7 +209,7 @@
         if(dati.risposte[3]== rispDate[2])
         {$("#r4").prop('checked', true);}
         //fine controllo se selezionata la risposta 3
-        
+
         //inizio controllo se confermata la risposta 4
         if(dati.risposte[0]== rispDate[3])
         {$("#r1").prop('checked', true);}
@@ -225,11 +225,11 @@
         //fine controllo se selezionata la risposta 4
         //alert(dati.infoDom[1]);
         if(dati.infoDom[1]){
-        	$("#commento").val(dati.infoDom[1]);
+          $("#commento").val(dati.infoDom[1]);
         }
         else
         {
-        	$("#commento").val("");
+          $("#commento").val("");
         }
       },
       error: function()
@@ -248,7 +248,7 @@
           return n.value;
         }).join(',');
         var idDom = $("#idDom").val();
-        
+
         //alert(output);
         // effettua l'alert del id domanda solo se la checkbox è stata flaggata
         //alert(domCliccata);
--- quiz.php
+++ quiz.php
@@ -1,36 +1,36 @@
- <?php
- 
- 		require ("connessione.php");
- 		$IDomanda = $_POST["id_domanda"];
+<?php
 
-                    $sql = " SELECT
+require ("connessione.php");
+$IDomanda = $_POST["id_domanda"];
+
+$sql = " SELECT
 w.id, w.r1,w.r2,w.r3,w.r4, r.fk_domanda,r.risposta, r.commento
 FROM _webquiz w
-    LEFT OUTER JOIN _webquizRisp r
-        ON w.id = r.fk_domanda WHERE w.id = '$IDomanda'";
-        
-           
-        
-        
-        
-        
-    	$ris = mysql_query($sql) or die("errore esecuzione query");
-        $dati = array();
+LEFT OUTER JOIN _webquizRisp r
+ON w.id = r.fk_domanda WHERE w.id = '$IDomanda'";
 
-        while($row = mysql_fetch_array($ris))
-        {
-        	//$dati['risposte'] = array($row["id"]."_".$row["r1"],$row["id"]."_".$row["r2"], $row["id"]."_".$row["r3"],$row["id"]."_".$row["r4"]);
-           	$dati['rispDate'] = array($row["fk_domanda"], $row["risposta"]);
-			$dati['risposte'] = array($row["r1"],$row["r2"], $row["r3"],$row["r4"]);
-			$dati['infoDom'] = array($row["id"],$row["commento"]);
 
-           /*$risp[] =  $row["r1"];
-           $risp[] =  $row["r2"];
-           $risp[] =  $row["r3"];
-           $risp[] =  $row["r4"];*/
-            
-        }
-        //inverte le domande
-        shuffle($dati['risposte']);
-        print_r(json_encode($dati));
-      ?>
+
+
+
+
+$ris = mysql_query($sql) or die("errore esecuzione query");
+$dati = array();
+
+while($row = mysql_fetch_array($ris))
+{
+  //$dati['risposte'] = array($row["id"]."_".$row["r1"],$row["id"]."_".$row["r2"], $row["id"]."_".$row["r3"],$row["id"]."_".$row["r4"]);
+  $dati['rispDate'] = array($row["fk_domanda"], $row["risposta"]);
+  $dati['risposte'] = array($row["r1"],$row["r2"], $row["r3"],$row["r4"]);
+  $dati['infoDom'] = array($row["id"],$row["commento"]);
+
+  /*$risp[] =  $row["r1"];
+  $risp[] =  $row["r2"];
+  $risp[] =  $row["r3"];
+  $risp[] =  $row["r4"];*/
+
+}
+//inverte le domande
+shuffle($dati['risposte']);
+print_r(json_encode($dati));
+?>
