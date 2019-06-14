144024fbe99f754db1c73ee3f0fdc09b4d57d065
Mario  Perna <marioperna@MBP-di-Mario.home-life.hub>
2019-06-15 01:02:41 +0200

    Insert comment area, when student can motivate the selected choices.

--- index.php
+++ index.php
@@ -1,9 +1,72 @@
 <html>
 <head>
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
+  <style>
+  	#container
+    {
+    	width:98%;
+        height:98%;
+		
+        background-color: lightgray;
+        color: black;
+        font-family: Verdana, Serif;
+        font-size:12px;
+        float: left;
+        margin-left: auto;
+        margin-right:auto;
+     }
+  	#container_domande{
+    	height: 70%;
+        width: 25%;
+        float: left;
+        background-color: lightgray;
+        color: black;
+        font-family: Verdana, Serif;
+        font-weight: bold;
+        font-size:12px;
+        padding: 1%;
+        border:1px solid black;
+    }
+    .domanda
+    {
+    	text-decoration: none;
+    }
+    #infoAzienda
+    {
+        border:1px solid black;
+    	height: 20%;
+        width: 25%;
+        float:left;
+        padding:1%;
+    }
+    #infoTest
+    {
+        border:1px solid black;
+    	height: 20%;
+        width: 67%;
+      	float:left;
+        padding:1%;
+    }
+  	#mostraRisposte{
+    	height: 70%;
+        width: 67%;
+        border:1px solid black;
+        float: left;
+        padding:1%;
+
+    }
+    .bloccoDomanda{
+    	
+        background-color: gray;
+       
+		
+    }
+  </style>
 </head>
 <body>
-
+<div id="container">
+	<div id="infoAzienda"></div>
+    <div id="infoTest"></div>
   <div id="container_domande">
     <?php
     require("connessione.php");
@@ -13,25 +76,59 @@
 
     while($row = mysql_fetch_array($ris))
     {
-      echo "<a href=javascript:void(0); id=".$row["id"]." OnClick=\"leggiDom('".$row["id"]."')\">".$row["domanda"]."</a>
-      <br>";
+      echo "
+      
+      <a href=javascript:void(0); id=".$row["id"]." class='domanda' OnClick=\"leggiDom('".$row["id"]."')\">".$row["domanda"]."</a>
+      <hr/>";
     }
     ?>
   </div>
   <div id="mostraRisposte">
-    <div id="rispDate"></div>
-    <input type="checkbox" name="r[]" id="r1" value=""><label id="lab_r1"></label> <br>
-    <input type="checkbox" name="r[]" id="r2" value=""><label id="lab_r2"></label>  <br>
-    <input type="checkbox" name="r[]" id="r3" value=""><label id="lab_r3"></label>  <br>
-    <input type="checkbox" name="r[]" id="r4"  value=""><label id="lab_r4"></label>  <br>
-    <input type="hidden" id="idDom" name="idDom" value="">
-  </div>
+      <input type="checkbox" name="r[]" id="r1" value=""><label id="lab_r1"></label> <br>
+      <input type="checkbox" name="r[]" id="r2" value=""><label id="lab_r2"></label>  <br>
+      <input type="checkbox" name="r[]" id="r3" value=""><label id="lab_r3"></label>  <br>
+      <input type="checkbox" name="r[]" id="r4"  value=""><label id="lab_r4"></label>  <br>
+      <input type="hidden" id="idDom" name="idDom" value="">
+      
+       <textarea rows="4" cols="50" id="commento" onfocusout="inserisciCommento()"></textarea> 
+   </div>
+</div>
 
   <script>
+  
+  $("#r1").hide();
+  $("#commento").hide();
+  $("#r2").hide();
+  $("#r3").hide();
+  $("#r4").hide();
+  
+  $("#commento").hide();
+  
+  function inserisciCommento()
+  {
+  	       
+       var idDomanda = $('#idDom').val();
+//inizio chimaata ajax
+       var commento = $('#commento').val();    
+        $.ajax({
+          type: "POST",
+          url: "risposte.php",
+          data: {id_domanda: idDomanda, commento:commento},
+          dataType: "JSON",
+          success: function(esito)
+          {
+				//$("#c").css('border-color', '#green');
+          },
+          error: function()
+          {
+            
+          }
+        });
+        //fine chiamata ajax
+       
+  }
   function leggiDom(a)
   {
-    //var  domCliccata = $(this).attr('id');
-    //alert(a);
     $.ajax
     ({
       type: "POST",
@@ -40,25 +137,38 @@
       dataType: "JSON",
       success: function(dati)
       {
-
+      $("#commento").val("");
+           $("#r1").show();
+ 			$("#r2").show();
+  			$("#r3").show();
+  	 		$("#r4").show();
+    		$("#commento").show();
+		//carica le label
         $("#lab_r1").text(dati.risposte[0]);
         $("#lab_r2").text(dati.risposte[1]);
         $("#lab_r3").text(dati.risposte[2]);
         $("#lab_r4").text(dati.risposte[3]);
-
+        //alert(dati.infoDom[1]);
+		//carica i valori contenuti nelle checkboxes
         $("#idDom").val(dati.infoDom[0]);
+        //$("#tipoDom").val(dati.infoDom[2]);
+        
         $("#r1").val(dati.risposte[0]);
         $("#r2").val(dati.risposte[1]);
         $("#r3").val(dati.risposte[2]);
         $("#r4").val(dati.risposte[3]);
-
+        
+		//splitta l'array e prende le risposte 
         var rispDate = dati.rispDate[1].split(",");
-
+        
+		//imposto tutte le checkbox che al cambio di domanda come non selezionate, poi il controllo 
+        //qui sotto effettuerà la verifica delle risposte date da utente
         $("#r1").prop('checked', false);
         $("#r2").prop('checked', false);
         $("#r3").prop('checked', false);
         $("#r4").prop('checked', false);
-
+        
+        //inizio controllo se confermata la risposta 1
         if(dati.risposte[0]== rispDate[0])
         {$("#r1").prop('checked', true);}
 
@@ -70,8 +180,9 @@
 
         if(dati.risposte[3]== rispDate[0])
         {$("#r4").prop('checked', true);}
-
-        ////////////////////////////////////risposte date 2
+        //fine controllo se selezionata la risposta 1
+        
+        //inizio controllo se confermata la risposta 2
         if(dati.risposte[0]== rispDate[1])
         {$("#r1").prop('checked', true);}
 
@@ -83,8 +194,9 @@
 
         if(dati.risposte[3]== rispDate[1])
         {$("#r4").prop('checked', true);}
-        /////////////////////////////////////////////////////////
-        ////////////////////////////////////risposte date 3
+        //fine controllo se selezionata la risposta 2
+        
+        //inizio controllo se confermata la risposta 3
         if(dati.risposte[0]== rispDate[2])
         {$("#r1").prop('checked', true);}
 
@@ -96,8 +208,9 @@
 
         if(dati.risposte[3]== rispDate[2])
         {$("#r4").prop('checked', true);}
-        /////////////////////////////////////////////////////////
-        ////////////////////////////////////risposte date 4
+        //fine controllo se selezionata la risposta 3
+        
+        //inizio controllo se confermata la risposta 4
         if(dati.risposte[0]== rispDate[3])
         {$("#r1").prop('checked', true);}
 
@@ -109,34 +222,15 @@
 
         if(dati.risposte[3]== rispDate[3])
         {$("#r4").prop('checked', true);}
-        /////////////////////////////////////////////////////////
-
-
-
-        //{}else{}
-
-
-
-        /*mi permette di controllare i dati se devo checckare la checkbox
-        */
-
-
-
-
-        //var r1 = dati.risposte[0].split("_");
-        //var r2 = dati.risposte[1].split("_");
-        //var r3 = dati.risposte[2].split("_");
-        //var r4 = dati.risposte[3].split("_");
-
-
-
-
-
-
-        //////////////////
-        //alert(risp);  qui devi vedere di fare il
-        //alert(risp.includes("nessuna delle precedenti"));
-        ////////////////////
+        //fine controllo se selezionata la risposta 4
+        //alert(dati.infoDom[1]);
+        if(dati.infoDom[1]){
+        	$("#commento").val(dati.infoDom[1]);
+        }
+        else
+        {
+        	$("#commento").val("");
+        }
       },
       error: function()
       {
@@ -144,35 +238,23 @@
       }
     });
   }
-
-
-
-
-
-
   $(document).ready(function()
   {
     //$('a').on("click", function(){
-
-    //
     //controllo se si verifica l'evento del click su una risposta (checkBox)
     $('input[type="checkbox"]').on("click", function(){
-
       if (this.checked) {
-
         var output = jQuery.map($(':checkbox[name=r\\[\\]]:checked'), function (n, i) {
           return n.value;
         }).join(',');
-
+        var idDom = $("#idDom").val();
+        
         //alert(output);
         // effettua l'alert del id domanda solo se la checkbox è stata flaggata
         //alert(domCliccata);
-        var idDom = $("#idDom").val();
-
         //var myarr = output.split("_");
-
         //alert("id_dom: "+myarr[0]+" rispo:"+ myarr[1]);
-        //
+        //inizio chimaata ajax
         $.ajax({
           type: "POST",
           url: "risposte.php",
@@ -180,30 +262,24 @@
           dataType: "JSON",
           success: function(esito)
           {
-
             //alert("operazione eseguita con successo");
-
           },
           error: function()
           {
             alert("Chiamata fallita, si prega di riprovare...");
           }
         });
-
-
-        //
+        //fine chiamata ajax
       }
       else
       {
-        //
         var output = jQuery.map($(':checkbox[name=r\\[\\]]:checked'), function (n, i) {
           return n.value;
         }).join(',');
-
         var idDom = $("#idDom").val();
         //var rispCancUtente = $(this).val();
         //var rispCancUtScomposta = rispCancUtente.split("_");
-
+        //inizio chiamata ajax
         $.ajax({
           type: "POST",
           url: "risposte.php",
@@ -220,14 +296,8 @@
             alert("Chiamata fallita, si prega di riprovare...");
           }
         });
-
-
-        //
-
+        //fine chiamata ajax
       }
-
-
-      ///
     });
   });
   </script>
--- risposte.php
+++ risposte.php
@@ -4,21 +4,21 @@ require("connessione.php");
 
 $IDomanda = $_POST["id_domanda"];
 $rispUtente =$_POST["prova"];
-
+$c =$_POST["commento"];
 
 
 if(!empty($rispUtente))
 {
   $sqlInserisciRisposta =
   "
-  INSERT INTO _webquizRisp (fk_domanda,risposta)
-  VALUES ('$IDomanda','$rispUtente')
+  INSERT INTO _webquizRisp (fk_domanda,risposta,commento)
+  VALUES ('$IDomanda','$rispUtente','')
   ON DUPLICATE KEY UPDATE risposta = '$rispUtente';
-
+  
   ";
-
+  
   $q = mysql_query($sqlInserisciRisposta) or die("errore esecuzione query".mysql_error());
-
+  
   if($q)
   {
     $esito["stato"] = "eseguito";
@@ -28,6 +28,29 @@ if(!empty($rispUtente))
     $esito["stato"] = "non eseguito";
   }
   print_r(json_encode($esito));
+}
+else if (isset($c))
+{
+  $sqlInserisciCommento =
+  "
+  INSERT INTO _webquizRisp (fk_domanda,risposta,commento)
+  VALUES ('$IDomanda','$rispUtente','$c')
+  ON DUPLICATE KEY UPDATE commento = '$c';
+  
+  ";
+  
+  $q = mysql_query($sqlInserisciCommento) or die("errore esecuzione query".mysql_error());
+  
+  if($q)
+  {
+    $esito["stato"] = "eseguito";
+  }
+  else
+  {
+    $esito["stato"] = "non eseguito";
+  }
+  print_r(json_encode($esito));
+
 }
 else
 {
